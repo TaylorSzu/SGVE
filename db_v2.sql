6bb566e88386c1190de43b8edfcd236f2947119f
Matheus Felix <matheusfelix.amorim@hotmail.com>
2024-05-08 09:58:53 -0300

    Update db_v2.1.sql

--- db_v2.sql
+++ db_v2.sql
@@ -36,3 +36,45 @@ select * from produto;
 call sp_add_produto(2,3,'Carro de brinquedo', 'Amox', 1,1,1,'Brinquedo', 10,20,30 ,'nada', 'algum texto');
 
 call sp_deletar_produto(2);
+
+CREATE PROCEDURE `sp_add_produto`(
+in vid int,
+ vcodigo int, 
+ vnome varchar(255), 
+ vfabricante varchar(255),
+ vestoque int, 
+ vestoque_minimo int,
+ vestoque_maximo int, 
+ vcategoria varchar(100),
+ vpeso float, 
+ vpreco_compra decimal(10,2), 
+ vpreco_venda decimal(10,2),
+ vunidade_medida varchar(50), 
+ vdescricao text)
+BEGIN
+insert into produto (id,codigo,nome,fabricante,estoque,estoque_minimo,estoque_maximo,categoria,peso,preco_compra,preco_venda,unidade_medida,descricao)
+values(vid,vcodigo,vnome,vfabricante,vestoque,vestoque_minimo,vestoque_maximo,vcategoria,vpeso,vpreco_compra,vpreco_venda,vunidade_medida,vdescricao);
+END
+
+CREATE PROCEDURE `sp_add_venda`(
+in vid int,
+ vproduto varchar(255),
+ vquantidade int,
+ vvalor_pago decimal(10,2),
+ vpagamento varchar(255))
+BEGIN
+insert into venda (id, produto, quantidade, valor_pago, pagamento)
+values(vid,vproduto,vquantidade,vvalor_pago,vpagamento);
+END
+
+CREATE  PROCEDURE `sp_deletar_produto`(vid int)
+BEGIN
+delete from produto
+where id = vid;
+END
+
+CREATE PROCEDURE `sp_deletar_venda`(vid int)
+BEGIN
+delete from venda
+where id = vid;
+END
